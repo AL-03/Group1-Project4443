9b0efa395d6243a7da71e7c0b076c50aa35e079d
alexalupo <alexa.lupo5@gmail.com>
2026-03-23 23:36:18 -0400

    Update UserDao.java

--- app/src/main/java/com/example/eecs4443project/data/dao/UserDao.java
+++ app/src/main/java/com/example/eecs4443project/data/dao/UserDao.java
@@ -20,4 +20,7 @@ public interface UserDao {
 
     @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
     LiveData<User> getUser(String username);
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password LIMIT 1")
+    User getUser(String username, String password);
 }
