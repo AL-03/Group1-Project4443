b21515d226d74a2e7aa64b047a0e5e3afaa66c17
alexalupo <alexa.lupo5@gmail.com>
2026-03-23 23:37:54 -0400

    Update UserDao.java

--- app/src/main/java/com/example/eecs4443project/data/dao/UserDao.java
+++ app/src/main/java/com/example/eecs4443project/data/dao/UserDao.java
@@ -14,13 +14,6 @@ public interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertUser(User user);
 
-    // COUNT will return 1 if credentials match, but 0 if they don't match
-    @Query("SELECT COUNT(*) FROM users WHERE username = :username AND password = :password")
-    LiveData<Integer> checkLogin(String username, String password);
-
-    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
-    LiveData<User> getUser(String username);
-
     @Query("SELECT * FROM users WHERE username = :username AND password = :password LIMIT 1")
     User getUser(String username, String password);
 }
